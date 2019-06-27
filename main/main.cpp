8a8c6a0199d4719efb7ef1816e55bdef3ab78fb6
yappy <superunyo@yahoo.co.jp>
2019-06-28 02:09:35 +0900

    Print ESP-IDF version

--- main/main.cpp
+++ main/main.cpp
@@ -1,4 +1,5 @@
 #include <M5Stack.h>
+#include <esp_system.h>
 
 #if CONFIG_FREERTOS_UNICORE
 #define ARDUINO_RUNNING_CORE 0
@@ -9,11 +10,14 @@
 // The setup routine runs once when M5Stack starts up
 void setup(){
 
-  // Initialize the M5Stack object
-  M5.begin();
+	// Initialize the M5Stack object
+	M5.begin();
 
-  // LCD display
-  M5.Lcd.print("Hello World");
+	// LCD display
+	M5.Lcd.println("Hello World");
+
+	M5.Lcd.print("ESP-IDF: ");
+	M5.Lcd.println(esp_get_idf_version());
 }
 
 // The loop routine runs over and over again forever
